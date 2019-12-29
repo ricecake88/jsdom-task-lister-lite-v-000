260e37f37ef4c9a542f3d28abd63a1ebeae5baa9
Grace Shih <vocalise@gmail.com>
2019-12-29 20:35:43 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@
    priority_form.setAttribute("name", "priorityForm")
    priority_form.appendChild(select_priority);
    select_priority.setAttribute("name", "dropdown");
-   select_priority.setAttribute("onchange", "this.priorityForm.submit()");
+   select_priority.setAttribute("onchange", "this.className.submit()");
    for (let i = 0; i < priorities.length; i++) {
       let option = document.createElement("option");
       option.value = priorities[i];
